a12425b99d0588d72306b21df0759b524818422a
YI <467678592@qq.com>
2018-02-27 12:05:17 +0800

    init

--- tests/testProcess.php
+++ tests/testProcess.php
@@ -6,33 +6,12 @@
  * Time: 1:06 PM
  */
 
-require  __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-
-//echo base64_encode(70);
-//echo base64_decode("MEUrMDA=");
-//echo rand()%4+6;
-//echo substr(system("date +%s%N"),2,17);
-//exit;
-
-$code = "WCb1bnAVp3lZQdDCU";
-//$client = new \GuzzleHttp\Client();
-//$response = $client->post("st.mybcdn.com:8395/iden",[
-//    "json"=>[
-//        "Id"=>"V0NiMWJuQVZwM2xaUWREQ1U="
-//    ]
-//]);
-//echo $response->getBody();
-//$service = new \Service\SendPackageService($code);
-//while (true){
-//
-//    $service->send();
-//    sleep(60);
-//}
-
-$service = new \Service\ProcessService();
+$code = isset($argv[1]) ? $argv[1] : "WCb1bnAVp3lZQdDCU";
+$service = new \Service\SendPackageService($code);
 while (true){
-    $service->addWork();
-    var_dump(\Service\ProcessService::$workNumbers);
-    sleep(20);
+
+    $service->send();
+    sleep(60);
 }
